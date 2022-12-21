7eff7c4b9c45b6ae0990903d7d14b6a64a47f6e9
Renaud Bresson <renaud.cci@gmail.com>
2022-12-21 10:29:52 +0100

    correction

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,17 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
+    if (x%5==0){
+        if (x%3==0){
+            console.log(`"fizzbuzz"`)
+        } else {
+            console.log(`"buzz"`)
+        }
+    } else if (x%3==0){
+        console.log(`"fizz"`)
+    } else {
+        console.log(x)
+    }
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
